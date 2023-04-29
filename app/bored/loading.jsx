f8239cd9be581e5e807398abe39a38983a21af2f
Anna Klimenko <any.klim@mail.ru>
2023-04-29 12:56:02 +0200

    cleaned up unnecessary

--- app/bored/loading.jsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <div className="w-full h-full flex justify-center align-center text-white">Loading...</div>
-}
--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,6 @@ import { Suspense } from "react";
 import Image from 'next/image'
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
-import { Bored } from './Bored';
-import { Songs } from "./Songs";
 
 async function getSongs() {
   const res = await fetch('https://itunes.apple.com/search?term=radiohead');
@@ -16,22 +14,7 @@ const Page = async () => {
 
   console.log(songs);
   return (<div className='bg-primary-black overflow-hidden h-full w-full'>
-  
-   
-  {/* <Suspense fallback={<h2 className="text-white">Loading feed...</h2>}>
-    <Bored />
-  </Suspense>
-   */}
-  <div className="w-full h-50 flex">
-    <Suspense fallback={<h2 className="text-white">Loading actions when you are bored...</h2>}>
-        <Bored />
-    </Suspense>
-  </div>
-  <div className="w-full h-50 flex">
-    <Suspense fallback={<h2 className="text-white">Loading songs...</h2>}>
-        <Songs />
-    </Suspense>
-  </div>
+    <About />
 </div>);
 }
 
